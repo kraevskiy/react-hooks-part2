f465ffc5cc38f2a394a4c910c9852482000b8e7f
kraevskiy <ilya.labelplatform@gmail.com>
2020-04-06 17:51:00 +0300

    some style

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import {BrowserRouter as Router} from "react-router-dom"
 import Routes from './routes'
-import TopBar from './components/topBar'
+import TopBar from './components/TopBar'
+import {CurrentUserState} from "./context/curentUser/currentUserState"
+import CurrentUserChecked from "./components/CurrentUserChecked"
 
 function App() {
 	return (
-		<Router>
-			<TopBar />
-			<Routes />
-		</Router>
+		<CurrentUserState>
+			<CurrentUserChecked>
+				<Router>
+					<TopBar/>
+					<Routes/>
+				</Router>
+			</CurrentUserChecked>
+		</CurrentUserState>
 	);
 }
 
